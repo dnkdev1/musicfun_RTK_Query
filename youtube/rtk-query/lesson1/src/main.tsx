34320ecc0d57500e93cd8d87e3bf8abb06f8918b
dnkdev1 <dnkdev@outlook.com>
2026-03-30 09:35:50 +0700

    feat: add environment config, store setup, types, UI and playlists query

--- youtube/rtk-query/lesson1/src/main.tsx
+++ youtube/rtk-query/lesson1/src/main.tsx
@@ -2,9 +2,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter } from 'react-router'
 import { App } from './app/ui/App/App.tsx'
+import { store } from '@/app/model/store.ts'
+import { Provider } from 'react-redux'
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>,
 )
