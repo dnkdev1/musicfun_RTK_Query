867d774048f6506565ab336bdecbe3d3fb29b4fc
dnkdev1 <dnkdev@outlook.com>
2026-04-12 16:02:02 +0700

    feat(auth): implement login, logout, and access/refresh token handling

--- youtube/rtk-query/lesson1/src/common/routing/Routing.tsx
+++ youtube/rtk-query/lesson1/src/common/routing/Routing.tsx
@@ -4,12 +4,14 @@ import { ProfilePage } from '@/features/auth/ui/ProfilePage/ProfilePage.tsx'
 import { PlaylistsPage } from '@/features/playlists/ui/PlaylistsPage.tsx'
 import { TracksPage } from '@/features/tracks/ui/TracksPage.tsx'
 import { Route, Routes } from 'react-router'
+import { OAuthCallback } from '@/features/auth/ui/OAuthCallback/OAuthCallback.tsx'
 
 export const Path = {
   Main: '/',
   Playlists: '/playlists',
   Tracks: '/tracks',
   Profile: '/profile',
+  OAuthRedirect: '/oauth/callback',
   NotFound: '*',
 } as const
 
@@ -19,6 +21,7 @@ export const Routing = () => (
     <Route path={Path.Playlists} element={<PlaylistsPage />} />
     <Route path={Path.Tracks} element={<TracksPage />} />
     <Route path={Path.Profile} element={<ProfilePage />} />
+    <Route path={Path.OAuthRedirect} element={<OAuthCallback />} />
     <Route path={Path.NotFound} element={<PageNotFound />} />
   </Routes>
 )
