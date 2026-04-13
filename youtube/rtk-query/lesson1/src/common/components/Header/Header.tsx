e9c69beaaba2541aa8945040b47529649bf4ab9d
dnkdev1 <dnkdev@outlook.com>
2026-04-13 09:16:02 +0700

    feat: add playlists to profile page

--- youtube/rtk-query/lesson1/src/common/components/Header/Header.tsx
+++ youtube/rtk-query/lesson1/src/common/components/Header/Header.tsx
@@ -1,5 +1,5 @@
 import { Path } from '@/common/routing'
-import { NavLink } from 'react-router'
+import { Link, NavLink } from 'react-router'
 import s from './Header.module.css'
 import { useGetMeQuery, useLogoutMutation } from '@/features/auth/api/authApi.ts'
 import { Login } from '@/features/auth/ui/Login/Login.tsx'
@@ -8,7 +8,7 @@ const navItems = [
   { to: Path.Main, label: 'Main' },
   { to: Path.Playlists, label: 'Playlists' },
   { to: Path.Tracks, label: 'Tracks' },
-  { to: Path.Profile, label: 'Profile' },
+  // { to: Path.Profile, label: 'Profile' },
 ]
 
 export const Header = () => {
@@ -28,11 +28,11 @@ export const Header = () => {
           ))}
         </ul>
       </nav>
-      {data && data.login}
 
       {data && (
         <div className={s.loginContainer}>
-          <p>{data.login}</p>
+          <Link to={Path.Profile}>{data.login}</Link>
+          <p></p>
           <button onClick={logoutHandler}>logout</button>
         </div>
       )}
