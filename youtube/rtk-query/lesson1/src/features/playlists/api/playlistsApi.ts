17bb8b82d06bf9c38d3df5e4ee71e5167b802f2c
dnkdev1 <dnkdev@outlook.com>
2026-04-13 13:22:02 +0700

    feat(streaming): implement fetch and update

--- youtube/rtk-query/lesson1/src/features/playlists/api/playlistsApi.ts
+++ youtube/rtk-query/lesson1/src/features/playlists/api/playlistsApi.ts
@@ -1,23 +1,53 @@
 import type {
   CreatePlaylistArgs,
   FetchPlaylistsArgs,
-  PlaylistData,
-  PlaylistsResponse,
+  PlaylistCreatedEvent,
+  PlaylistUpdatedEvent,
   UpdatePlaylistArgs,
 } from '@/features/playlists/api/playlistsApi.types.ts'
 import { baseApi } from '@/app/api/baseApi.ts'
 import type { Images } from '@/common/types'
 import { playlistCreateResponseSchema, playlistsResponseSchema } from '@/features/playlists/model/playlists.schemas.ts'
-import { errorToast, withZodCatch } from '@/common/utils'
+import { withZodCatch } from '@/common/utils'
 import { imagesSchema } from '@/common/schemas/schemas.ts'
-
+import { subscribeToEvent } from '@/common/socket/subscribeToEvent.ts'
+import { SOCKET_EVENTS } from '@/common/constants/constants.ts'
 
 export const playlistsApi = baseApi.injectEndpoints({
   endpoints: (build) => ({
     fetchPlaylists: build.query({
       query: (params: FetchPlaylistsArgs) => ({ url: `playlists`, params }),
       ...withZodCatch(playlistsResponseSchema),
-      skipSchemaValidation: process.env.NODE_ENV === 'production',
+      keepUnusedDataFor: 0, // 👈 очистка сразу после размонтирования
+      async onCacheEntryAdded(_arg, { updateCachedData, cacheDataLoaded, cacheEntryRemoved }) {
+        // Ждем разрешения начального запроса перед продолжением
+        await cacheDataLoaded
+
+        const unsubscribes = [
+          subscribeToEvent<PlaylistCreatedEvent>(SOCKET_EVENTS.PLAYLIST_CREATED, (msg) => {
+            const newPlaylist = msg.payload.data
+            updateCachedData((state) => {
+              state.data.pop()
+              state.data.unshift(newPlaylist)
+              state.meta.totalCount = state.meta.totalCount + 1
+              state.meta.pagesCount = Math.ceil(state.meta.totalCount / state.meta.pageSize)
+            })
+          }),
+          subscribeToEvent<PlaylistUpdatedEvent>(SOCKET_EVENTS.PLAYLIST_UPDATED, (msg) => {
+            const newPlaylist = msg.payload.data
+            updateCachedData((state) => {
+              const index = state.data.findIndex((playlist) => playlist.id === newPlaylist.id)
+              if (index !== -1) {
+                state.data[index] = { ...state.data[index], ...newPlaylist }
+              }
+            })
+          }),
+        ]
+
+        // CacheEntryRemoved разрешится, когда подписка на кеш больше не активна
+        await cacheEntryRemoved
+        unsubscribes.forEach((unsubscribe) => unsubscribe())
+      },
       providesTags: ['Playlist'],
     }),
 
@@ -127,7 +157,6 @@ export const {
   useUploadPlaylistCoverMutation,
   useDeletePlaylistCoverMutation,
 } = playlistsApi
-
 
 // export const playlistsApi = baseApi.injectEndpoints({
 //   endpoints: (build) => ({
